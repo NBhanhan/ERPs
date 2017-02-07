f5cf014db44ff39e2c2d5cfe96ac431374b793a5
NBhanhan <nbhanhan@gmail.com>
2017-02-07 00:16:45 +0000

    home page

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample App') - ERP Supplier</title>
+    <title>@yield('title', 'Sample App') - ERP SUPPLIER</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+    <header class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+          <a href="/" id="logo">SUPPLIERS</a>
+          <nav>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="/help">HELP</a></li>
+              <li><a href="#">LOGIN</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.default')
 @section('title', 'HOME')
-
 @section('content')
-  <h1>HOME</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      ERP Suppliers
+    </p>
+    <p>
+      Start from here
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">STORE</a>
+      <a class="btn btn-lg btn-success" href="#" role="button">SEARCH</a>
+      <a class="btn btn-lg btn-success" href="#" role="button">UPDATE</a>
+      <a class="btn btn-lg btn-success" href="#" role="button">DESTROY</a>
+    </p>
+  </div>
 @stop
